093a019d216f8a0b10821d117614bc6614b24877
ladiersal <deotrerandycup@gmail.com>
2022-08-01 22:59:34 +0300

    Updates

---
+++ precache-manifest.985d033a811218e43aaed91a0a159c15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "73f8800dae9c20352c7b",
+    "url": "/weather-app/static/css/main.5fc40932.chunk.css"
+  },
+  {
+    "revision": "73f8800dae9c20352c7b",
+    "url": "/weather-app/static/js/main.73f8800d.chunk.js"
+  },
+  {
+    "revision": "9269d79d26247a8cbb76",
+    "url": "/weather-app/static/js/1.9269d79d.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "d59b0a1f00e631b0458e1bea022e7920",
+    "url": "/weather-app/static/media/sunset.d59b0a1f.jpg"
+  },
+  {
+    "revision": "49c371f2bebeb84e873a4e090b9a2676",
+    "url": "/weather-app/index.html"
+  }
+];
